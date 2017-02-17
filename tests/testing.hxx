a9d4d2b5a55612c6f074d8fac3f9f4ce709753ce
flopez <florent.lopez@stfc.ac.uk>
2017-02-17 16:47:02 +0000

    added source file with testing routiens

---
+++ tests/testing.hxx
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstring>
+#include <cstdlib>
+
+namespace spldlt {
+
+   class SpldltOpts {
+
+   public:
+      SpldltOpts() : ncpu(1), m(32), n(32),
+                     nb(32), ib(32) {}
+
+      void parse_opts(int argc, char** argv) {
+         
+         for( int i = 1; i < argc; ++i ) {
+
+            if ( !strcmp("--nb", argv[i]) && i+1 < argc ) {
+               nb = std::atoi( argv[++i] );
+            }
+            else if ( !strcmp("--m", argv[i]) && i+1 < argc ) {
+               m =  std::atoi( argv[++i] );
+            }
+            else if ( !strcmp("--n", argv[i]) && i+1 < argc ) {
+               n =  std::atoi( argv[++i] );
+            }
+            else if ( !strcmp("--ncpu", argv[i]) && i+1 < argc ) {
+               ncpu =  std::atoi( argv[++i] );
+            }
+
+         }
+         
+      }
+
+      int ncpu; // no cpu
+      int m; // no rows in matrix
+      int n; // no columns in matrix
+      int nb; // block size
+      int ib; // inner block size
+      
+   };
+}
