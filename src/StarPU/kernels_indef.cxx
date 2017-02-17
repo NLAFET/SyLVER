dc93a2be22beef44fdc482b9c9157cc0cb3ad056
flopez <florent.lopez@stfc.ac.uk>
2017-02-17 16:58:20 +0000

    added missing file

---
+++ src/StarPU/kernels_indef.cxx
@@ -0,0 +1,23 @@
+#include <starpu.h>
+
+namespace spldlt { namespace starpu {
+      
+      /* factor_block_app StarPU codelet */
+      struct starpu_codelet cl_factor_block_app;
+
+      /* applyN_block_app StarPU codelet */
+      struct starpu_codelet cl_applyN_block_app;      
+
+      /* applyT_block_app StarPU codelet */
+      struct starpu_codelet cl_applyT_block_app;      
+      
+      /* updateT_block_app StarPU codelet */
+      struct starpu_codelet cl_updateT_block_app;      
+
+      /* updateN_block_app StarPU codelet */
+      struct starpu_codelet cl_updateN_block_app;      
+
+      /* adjust StarPU codelet */
+      struct starpu_codelet cl_adjust;
+      
+}} /* namespaces spldlt::starpu  */
