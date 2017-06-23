499d15cfe175f5321061113187a27135b68e2255
Florent Lopez <flopez@b-an01.hpc2n.umu.se>
2017-06-23 17:19:27 +0200

    added script for launching tests and jobs

---
+++ aux/hpc2n_run_tests_job.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+# Project/Account
+#SBATCH -A SNIC2016-1-536
+# Request exclusive access to the node
+#SBATCH --exclusive
+# One task
+#SBATCH -n 1
+# Number of cores per task
+#SBATCH -c 28
+# Runtime of this jobs is less then 10 hours.
+#SBATCH --time=02:00:00
+# Job name
+#SBATCH -J spldlt_tests
+# Ouput
+#SBATCH --error=job.err
+#SBATCH --output=job.out
+
+# srun ./hpc2n_run_tests_job_aux.sh
+
+echo "[HPC2N run_tests SpLDLT]"
+
+# Choose build
+# - starpu: SpLLT vesion StarPU (exploit tree prunning by default)
+# - gnu_omp: SpLLT OpenMP version. GNU compiler 
+# - intel_omp: SpLLT OpenMP version. Intel compiler 
+# - parsec: PaRSEC version of SpLLT
+
+build="starpu"
+
+build_dir=`pwd`
+id=`whoami`
+outdir=../data
+#outsuffix="_NOSUB"
+outsuffix=
+
+echo "[HPC2N run_tests SpLDLT] build dir: $build_dir"
+#matrices=(JGD_Trefethen/Trefethen_20000)
+
+mkdir -p $outdir
+
+case $build in
+    stf)
+        mkdir -p $outdir/stf
+        ;;
+    parsec)
+        mkdir -p $outdir/parsec
+        mkdir -p $outdir/parsec/traces
+        # make clean
+        # make parsec
+        ;;
+    starpu)
+        mkdir -p $outdir/starpu
+        mkdir -p $outdir/starpu/traces
+        ;;
+    starpu_nested_stf)
+        mkdir -p $outdir/starpu_nested_stf
+        mkdir -p $outdir/starpu_nested_stf/traces
+        ;;
+    gnu_omp|gnu_omp_prune)
+        mkdir -p $outdir/omp
+        mkdir -p $outdir/omp/gnu
+        mkdir -p $outdir/omp/gnu/traces
+        ;;
+    intel_omp|intel_omp_prune)
+        mkdir -p $outdir/omp
+        mkdir -p $outdir/omp/intel
+        mkdir -p $outdir/omp/intel/traces
+        ;;
+    ma87)
+        mkdir -p $outdir/ma87
+        ;;
+esac
+
+ncpu_list=(27)
+nb_list=(256 384 512 768 1024 1536)
+nemin_list=(32)
+
+. ./run_tests_loop.sh
+
+# End of submit file
---
+++ aux/run_tests_loop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+hostname=`hostname`
+echo "[run_tests] compute node: $hostname"
+
+# list loaded modules
+module list
+
+for matrix in `cat list.matrix`
+do
+    matname=`echo $matrix | sed -e 's/\//_/g'`
+    echo "[run_tests] test matrix: $matname"
+    # set up matrix
+    echo "[run_tests] extract matrix: $matname"
+    ./prep.sh $matrix
+
+    for ncpu in ${ncpu_list[@]}
+    do
+        echo "[run_tests] ncpu: $ncpu"
+
+        for nb in ${nb_list[@]}
+        do
+            echo "[run_tests] nb: $nb"
+
+            for nemin in ${nemin_list[@]}
+            do  
+                echo "[run_tests] nemin: $nemin"
+
+                # ./run_ma87
+                echo "[run_tests] run SPLLT_STARPU"
+                rm -rf $trace_dir/$prof_file
+                # just to make sure
+                export OMP_NUM_THREADS=1
+                case $build in
+                    starpu)
+                        ../builds/starpu/spllt_test --posdef --ncpu ${ncpu} --nb ${nb} --nemin ${nemin} > $outdir/starpu/${matname}_NCPU-${ncpu}_NB-${nb}_NEMIN-${nemin}${outsuffix}
+                        ;;
+                esac
+
+                if [ -f $trace_dir/$prof_file ];
+                then
+                    mv $trace_dir/$prof_file $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.prof
+                    starpu_fxt_tool -c -i $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.prof -o $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.trace
+                fi
+            done
+        done
+    done
+done
