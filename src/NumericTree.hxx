3abc4f48cd8de1dadfa62e8ffc6f84cf1029fffd
flopez <florent.joan.lopez@gmail.com>
2018-03-01 15:08:30 +0000

    added task_wait_for_all

--- src/NumericTree.hxx
+++ src/NumericTree.hxx
@@ -168,9 +168,10 @@ namespace spldlt {
 //             starpu_task_wait_for_all();
 // #endif
          }
-         // #if defined(SPLDLT_USE_STARPU)
-         //          starpu_task_wait_for_all();
-         // #endif         
+
+#if defined(SPLDLT_USE_STARPU)
+         starpu_task_wait_for_all();
+#endif         
          // Allocate mapping array
          // int *map = new int[symb_.n+1];
 
