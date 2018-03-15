0e40d3ed3ed56cde80e6fefcef3ebd052060ff14
flopez <florent.joan.lopez@gmail.com>
2018-03-15 14:45:18 +0000

    added missing file

---
+++ tests/drivers/factor_node_test.cxx
@@ -0,0 +1,22 @@
+// SpLDLT
+#include "testing.hxx"
+#include "testing_factor_node_indef.hxx"
+
+using namespace spldlt;
+using namespace spldlt::tests;
+
+int main(int argc, char** argv) {
+
+   printf("[factor_node_test]\n");
+
+   spldlt::SpldltOpts opts;
+   opts.parse_opts(argc, argv);
+
+   printf("[factor_node_test] Matrix %dx%d\n", opts.m, opts.n);
+   printf("[factor_node_test] blksz = %d, ib:%d\n", opts.nb, opts.ib);
+   printf("[factor_node_test] # CPU = %d\n", opts.ncpu);
+
+   // factor_node_indef_test<double, 32, false>(0.01, 1e-20, true, false, opts.m, opts.n, opts.nb, opts.ncpu);
+   // No delays
+   factor_node_indef_test<double, 32, false>(0.01, 1e-20, false, false, opts.m, opts.n, opts.nb, opts.ncpu);   
+}
