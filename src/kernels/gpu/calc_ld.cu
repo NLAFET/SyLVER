1555e423995815a3000508691cfec0bad28aad04
Florent Lopez <florent.lopez@stfc.ac.uk>
2018-06-12 13:43:34 -0400

    minor

--- src/kernels/gpu/calc_ld.cu
+++ src/kernels/gpu/calc_ld.cu
@@ -1,18 +1,28 @@
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 
-namespace spldlt {
+#define CALC_LD_NTX 8  // Number of threads x
+
+namespace /* anon */ {
 
    __global__ void
    cu_calc_ld(
+         int m,
+         int n,
          double *const d) {
 
    }
+
+}
+
+namespace spldlt {
    
    extern "C" {
       
       void calc_ld(
             const cudaStream_t stream,
+            int m,
+            int n,
             double *const d,
             ) {
 
