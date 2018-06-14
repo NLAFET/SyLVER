4000932fd4fc8bcef54def029a278aaf4ae066e5
Florent Lopez <florent.lopez@stfc.ac.uk>
2018-06-14 17:48:38 -0400

    minor

--- src/kernels/gpu/calc_ld.cu
+++ src/kernels/gpu/calc_ld.cu
@@ -1,11 +1,12 @@
 #include<stdio.h>
+#include <limits>
 
 #include <cuda_runtime.h>
 #include <cuda_runtime_api.h>
 
 // #define BLOCK_SIZE 128  // Number of threads
-#define BLOCK_SIZE 16  // Number of threads
-// #define BLOCK_SIZE 32  // Number of threads
+// #define BLOCK_SIZE 16  // Number of threads
+#define BLOCK_SIZE 32  // Number of threads
 
 namespace /* anon */ {
 
@@ -25,15 +26,20 @@ namespace /* anon */ {
 
       for (int col = ty + by * blockDim.y; col < n; col += blockDim.y * gridDim.y) {
 
+         // if (tx == 0) {
+         //    printf("[cu_calc_ld] col = %d, d[2*col] = %f\n", col, d[2*col]);
+         // }
+         
          // Check if we are halfway trhough a 2x2 pivot
          // if (col+1 < n && !std::isfinite(d[2*col]))
          //    continue;
          
-         if(col+1==n || (std::isfinite(d[2*col+2]) && std::isfinite(d[2*col]))) {
+         if((col+1==n || std::isfinite(d[2*col+2])) && std::isfinite(d[2*col])) {
             // 1x1 pivot
 
-            // printf("[cu_calc_ld] 1x1\n");
-
+            // printf("[cu_calc_ld] 1x1 ty = %d\n", ty);
+            // printf("[cu_calc_ld] tx = %d, ty = %d\n", tx, ty);
+            
             T d11 = d[2*col];
             if(d11 != 0.0) d11 = 1/d11;
 
@@ -43,9 +49,13 @@ namespace /* anon */ {
          }
          // else {
          else if (std::isfinite(d[2*col])) {
+         // else if (d[2*col]==std::numeric_limits<T>::infinity()) {
             // 2x2 pivot
 
-            // printf("[cu_calc_ld] 2x2\n");
+            // if (tx == 0)
+            // printf("[cu_calc_ld] TETETETETTET ty = %d\n", ty);
+            // printf("[cu_calc_ld] tx = %d, ty = %d\n", tx, ty);
+            // printf("[cu_calc_ld] col = %d\n");
 
             T d11 = d[2*col];
             T d21 = d[2*col+1];
@@ -65,25 +75,31 @@ namespace /* anon */ {
                ld[(col+1)*ldld+row] = -d21*a1 + d11*a2;
             }
 
-            // T d11 = d[2*col-2];
-            // T d21 = d[2*col-1];
-            // T d22 = d[2*col+1];
+         }
+         // else {
 
-            // // printf("[cu_calc_ld] d11 = %f, d21 = %f, d22 = %f\n", d11, d21, d22);
+         //    T d11 = d[2*col-2];
+         //    T d21 = d[2*col-1];
+         //    T d22 = d[2*col+1];
 
-            // T det = d11*d22 - d21*d21;
-            // d11 = d11/det;
-            // d21 = d21/det;
-            // d22 = d22/det;
-            // for(int row = tx + bx * blockDim.x; row < m; row += blockDim.x * gridDim.x) {
-            //    T a1, a2;
-            //    a1 = l[(col-1)*ldl+row];
-            //    a2 = l[col*ldl+row];
-            //    ld[(col-1)*ldld+row] =  d22*a1 - d21*a2;
-            //    ld[col*ldld+row]     = -d21*a1 + d11*a2;
-            // }
+         //    // if (tx == 0) {
+         //    //    printf("[cu_calc_ld] ty = %d\n", ty);
+         //    //    printf("[cu_calc_ld] d11 = %f, d21 = %f, d22 = %f\n", d11, d21, d22);
+         //    // }
+               
+         //    T det = d11*d22 - d21*d21;
+         //    d11 = d11/det;
+         //    d21 = d21/det;
+         //    d22 = d22/det;
+         //    for(int row = tx + bx * blockDim.x; row < m; row += blockDim.x * gridDim.x) {
+         //       T a1, a2;
+         //       a1 = l[(col-1)*ldl+row];
+         //       a2 = l[col*ldl+row];
+         //       // ld[(col-1)*ldld+row] =  d22*a1 - d21*a2;
+         //       ld[col*ldld+row]     = -d21*a1 + d11*a2;
+         //    }
 
-         } 
+         // } 
       }
 
    }
