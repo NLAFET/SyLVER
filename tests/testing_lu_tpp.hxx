c2537902109a7917bfa37ae50dacfdff10413ed7
flopez <florent.joan.lopez@gmail.com>
2018-12-11 10:52:30 +0000

    added missing test routine for LU  with tpp

---
+++ tests/testing_lu_tpp.hxx
@@ -0,0 +1,145 @@
+#pragma once
+
+// SpLDLT
+#include "kernels/lu_tpp.hxx"
+#include "kernels/lu_nopiv.hxx"
+
+// STD
+#include <chrono>
+
+namespace spldlt {
+   namespace tests {
+
+      /// @param u threshold parameter
+      /// @param m matrix order
+      /// @param k number of rows/columns to be eliminated
+      template<typename T>
+      int lu_tpp_test(T u, int m, int k, bool diagdom, bool check) {
+
+         bool failed = false;
+
+         printf("[lu_tpp_test] u = %e\n", u);
+         printf("[lu_tpp_test] m = %d\n", m);
+         printf("[lu_tpp_test] k = %d\n", k);
+         printf("[lu_tpp_test] diagdom = %d, check = %d\n", diagdom, check);
+         ASSERT_TRUE(m >= k);
+
+         // Generate test matrix
+         int lda = spral::ssids::cpu::align_lda<T>(m);
+         T* a = nullptr;
+         T* b = nullptr;
+
+         // Factors
+         T* lu = new T[m*lda]; 
+
+         if (check) {
+            
+            a = new T[m*lda];
+
+            if (diagdom) gen_unsym_diagdom(m, a, lda);
+            else         gen_mat(m, m, a, lda);
+
+            b = new T[m];
+            gen_unsym_rhs(m, a, lda, b);
+
+            memcpy(lu, a, lda*m*sizeof(T));
+         }
+         else {
+
+            if (diagdom) gen_unsym_diagdom(m, lu, lda);
+            else         gen_mat(m, m, lu, lda);
+         }
+
+         // Initialize permutations
+         int *rperm = new int[m];
+         int *cperm = new int[m];
+         for (int i=0; i<m; ++i) {
+            rperm[i] = i;
+            cperm[i] = i;
+         }
+
+         int nelim = 0;
+         auto start = std::chrono::high_resolution_clock::now();
+         // Perform (partial) LU factor with threshold partial pivoting
+         nelim = lu_tpp_factor(u, m, k, rperm, cperm, lu, lda);
+         auto end = std::chrono::high_resolution_clock::now();
+         long ttotal = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+
+         printf("[lu_tpp_test] nelim = %d out of k = %d columns\n", nelim, k);
+
+         // Print permutations
+         printf("rperm = \n");
+         for (int i=0; i<m; ++i)
+            printf(" %d ", rperm[i]);
+         printf("\n");
+
+         printf("cperm = \n");
+         for (int i=0; i<m; ++i)
+            printf(" %d ", cperm[i]);
+         printf("\n");
+
+         if (m > k) {
+            // TODO
+            // Factor reminder
+            // ...
+         }
+
+         // Check solution
+         if (check) {
+
+            int nrhs = 1;
+            int ldsoln = m;
+            T *soln = new T[nrhs*ldsoln];
+
+            // Setup permuted rhs 
+            T *pb = new T[m];
+            for (int i=0; i<m; ++i)
+               for (int r=0; r<nrhs; ++r)
+                  pb[r*ldsoln+i] = b[r*ldsoln+rperm[i]];
+
+            // // Setup permuted x
+            // T *psoln = new T[m];
+            // for (int i=0; i<m; ++i)
+            //    for (int r=0; r<nrhs; ++r)
+            //       psoln[r*ldsoln+i] = soln[r*ldsoln+cperm[i]];
+
+            // Copy rhs into solution vector
+            for(int r=0; r<nrhs; ++r)
+               memcpy(&soln[r*ldsoln], pb, m*sizeof(T));
+
+            // Perform solve
+            // Fwd substitutuion
+            lu_nopiv_fwd(m, k, lu, lda, nrhs, soln, ldsoln);
+            // Bwd substitutuion
+            lu_nopiv_bwd(m, k, lu, lda, nrhs, soln, ldsoln);
+
+            // Permute x
+            T *psoln = new T[m];
+            for (int i=0; i<m; ++i)
+               for (int r=0; r<nrhs; ++r)
+                  psoln[r*ldsoln+i] = soln[r*ldsoln+cperm[i]];
+
+            // Calculate bwd error
+            double bwderr = unsym_backward_error(
+                  m, k, a, lda, b, nrhs, psoln, ldsoln);
+
+            printf("bwderr = %le\n", bwderr);
+            EXPECT_LE(bwderr, 5e-14);
+            printf("factor time (s) = %e\n", 1e-9*ttotal);
+            
+         }
+
+         // Cleanup
+         if (check) {
+            delete[] a;
+            delete[] b;
+         }
+
+         delete[] lu;
+         delete[] rperm;
+         delete[] cperm;
+
+      }
+      
+   } // end of namespace tests
+} // end of namespace spldlt
