9a550b6c2f6821967694ab266e8262667eca12ab
flopez <florent.joan.lopez@gmail.com>
2018-12-13 20:13:20 +0000

    added more tests

--- tests/drivers/kernel_test.cxx
+++ tests/drivers/kernel_test.cxx
@@ -93,11 +93,12 @@ int main(int argc, char** argv) {
    struct spral::ssids::cpu::cpu_factor_options options;
    options.cpu_block_size = 4;
    // factor_node_unsym_test<double, spral::test::AlignedAllocator<double>>(options, 4, 4, 1, 0, true, true); // Diagonally dominant
-   factor_node_unsym_test<double, spral::test::AlignedAllocator<double>>(options, 4, 4, 1, 0, false, true); // Diagonally dominant
+   // factor_node_unsym_test<double, spral::test::AlignedAllocator<double>>(options, 4, 4, 1, 0, false, true); // Diagonally dominant
    // options.cpu_block_size = 8;
    // factor_node_unsym_test<double, spral::test::AlignedAllocator<double>>(options, 8, 8, 1, 0, true, true); // Diagonally dominant
-   // options.cpu_block_size = 4;
+   options.cpu_block_size = 4;
    // factor_node_unsym_test<double, spral::test::AlignedAllocator<double>>(options, 8, 8, 1, 0, true, true); // Diagonally dominant
+   factor_node_unsym_test<double, spral::test::AlignedAllocator<double>>(options, 16, 16, 1, 0, true, true); // Diagonally dominant
 
    // nerr += run_factor_node_indef_tests();
    // nerr += run_form_contrib_tests();
