918cf462e6c65cba88c722837d8176cbbbbe36cc
flopez <florent.lopez@stfc.ac.uk>
2020-02-07 15:27:31 -0500

    minor modif in CMake

--- cmake/build_helpers.cmake
+++ cmake/build_helpers.cmake
@@ -8,5 +8,64 @@ function(sylver_add_example example)
     ${CMAKE_BINARY_DIR}/src)
 
   target_link_libraries(${example_name} PRIVATE sylver)
-  target_link_libraries(${example_name} PRIVATE ${LIBS})  
+  target_link_libraries(${example_name} PRIVATE ${LIBS})
 endfunction()
+
+include(CheckCXXCompilerFlag)
+
+#
+# Tests whether the cxx compiler understands a flag.
+# If so, add it to 'variable'.
+#
+# Usage:
+#     enable_cxx_flag_if_supported(variable flag)
+#
+
+MACRO(enable_cxx_compiler_flag_if_supported _variable _flag)
+  #
+  # Clang is too conservative when reporting unsupported compiler flags.
+  # Therefore, we promote all warnings for an unsupported compiler flag to
+  # actual errors with the -Werror switch:
+  #
+  IF(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    SET(_werror_string "-Werror ")
+  ELSE()
+    SET(_werror_string "")
+  ENDIF()
+
+  STRING(STRIP "${_flag}" _flag_stripped)
+  SET(_flag_stripped_orig "${_flag_stripped}")
+
+  #
+  # Gcc does not emit a warning if testing -Wno-... flags which leads to
+  # false positive detection. Unfortunately it later warns that an unknown
+  # warning option is used if another warning is emitted in the same
+  # compilation unit.
+  # Therefore we invert the test for -Wno-... flags:
+  #
+  IF(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    STRING(REPLACE "-Wno-" "-W" _flag_stripped "${_flag_stripped}")
+  ENDIF()
+
+  # unset(CXX_COMPILER_FLAG_SUPPORTED)
+  IF(NOT "${_flag_stripped}" STREQUAL "")
+    STRING(REGEX REPLACE "^-" "" _flag_name "${_flag_stripped}")
+    STRING(REPLACE "," "" _flag_name "${_flag_name}")
+    STRING(REPLACE "-" "_" _flag_name "${_flag_name}")
+    STRING(REPLACE "+" "_" _flag_name "${_flag_name}")
+    CHECK_CXX_COMPILER_FLAG(
+      "${_werror_string}${_flag_stripped}"
+      SYLVER_HAVE_FLAG_${_flag_name}
+      # CXX_COMPILER_FLAG_SUPPORTED
+      )
+    # message("CXX_COMPILER_FLAG_SUPPORTED: ${CXX_COMPILER_FLAG_SUPPORTED}")
+    message("Is flag supported: ${SYLVER_HAVE_FLAG_${_flag_name}}")
+
+    set(SYLVER_HAVE_FLAG_${_flag_name} ${CXX_COMPILER_FLAG_SUPPORTED})
+    IF(SYLVER_HAVE_FLAG_${_flag_name})
+      message("TETETETETET")
+      SET(${_variable} "${${_variable}} ${_flag_stripped_orig}")
+      STRING(STRIP "${${_variable}}" ${_variable})
+    ENDIF()
+  ENDIF()
+ENDMACRO()
