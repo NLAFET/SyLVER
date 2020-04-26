7045d0a6f8ac6bd0857127ac70d835885a51596e
flopez <florent.lopez@stfc.ac.uk>
2020-04-26 19:40:06 -0400

    changed ci build

--- ci/makebuild.sh
+++ ci/makebuild.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir builds
-mkdir builds/master-starpu
-cd builds/starpu
-cmake ../.. -D SYLVER_RUNTIME=StarPU -D SYLVER_ENABLE_CUDA=OFF -D SYLVER_ENABLE_OMP=OFF -D SYLVER_SPRAL_USE_INTERNAL=ON -D SYLVER_BUILD_UNIT_TESTS=ON
+mkdir builds/sylver-starpu
+cd builds/sylver-starpu
+cmake ../.. -D SYLVER_RUNTIME=StarPU -D SYLVER_ENABLE_CUDA=OFF -D SYLVER_ENABLE_OMP=OFF -D SYLVER_SPRAL_USE_INTERNAL=ON \
+      -D SYLVER_BUILD_UNIT_TESTS=ON
 # make
 RESULT=$?
 [ $RESULT -ne 0 ] && exit 1
